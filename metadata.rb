1940eec9647059f8c46c8c4df8597fa6693e63a8
Andreas Wagner <whysthatso@gmx.de>
2015-03-26 14:48:17 +0200

    bumping bugfix version number cause i like it

--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'andreas.wagner@cashongo.co.uk'
 license          'Apache 2.0'
 description      'Installs/Configures cog_newrelic'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.4'
+version          '0.1.5'
 
 depends 'chef-vault', '= 1.2.5'
