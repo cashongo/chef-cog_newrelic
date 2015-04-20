134c82777090f26090f59407294bac2ad8637ff3
whysthatso <whysthatso@gmx.de>
2015-04-20 14:00:44 +0300

    bugfixes

--- recipes/plugin_mysql.rb
+++ recipes/plugin_mysql.rb
@@ -53,7 +53,7 @@ bash 'extract_plugin' do
 end
 
 Chef::Log.info("#{mysql_access}")
-Chef::Log.info("#{mysql_access['newrelic']}")
+Chef::Log.info("#{newrelic_license}")
 
 template "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}/config/plugin.json" do
   source    'newrelic-plugin-mysql.json.cfg.erb'
