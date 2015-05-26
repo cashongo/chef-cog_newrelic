272d58263745268381fe2691ff804180d088ac1e
Lauri Jesmin <lauri.jesmin@cashongo.co.uk>
2015-05-26 11:42:06 +0300

    Systemd support for Rhel / Centos 7 mysql plugin

--- recipes/plugin_mysql.rb
+++ recipes/plugin_mysql.rb
@@ -14,6 +14,12 @@ mysql_access      = chef_vault_item("mysql_passwords_#{node.chef_environment}", 
 # plugin dependencies
 package node['cog_newrelic']['plugin_mysql']['java_package']
 
+if (node['cog_newrelic']['plugin_mysql']['init_style'] == "systemd" ) then
+  notifyname = 'service[newrelic-plugin-mysql]'
+else
+  notifyname = 'runit_service[newrelic-plugin-mysql]'
+end
+
 directory node['cog_newrelic']['plugin-log-path'] do
   recursive true
   mode      0777
@@ -30,7 +36,8 @@ end
 
 directory node['cog_newrelic']['plugin-path'] do
   recursive true
-  mode      0777
+  mode      0700
+  owner node['cog_newrelic']['daemon_user']
 
   action :create
 end
@@ -46,22 +53,22 @@ bash 'extract_plugin' do
   cwd Chef::Config[:file_cache_path]
   code <<-EOH
     tar xzf #{Chef::Config[:file_cache_path]}/newrelic-mysql-#{node['cog_newrelic']['plugin_mysql']['version']}.tar.gz -C #{node['cog_newrelic']['plugin-path']}
-    chmod 0644 "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}"
+    chmod 0755 "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}"
     EOH
 
-  not_if { ::File.exists?("#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_gearman']['version']}") }
+  not_if { ::File.exists?("#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}") }
 end
 
 template "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}/config/plugin.json" do
   source    'newrelic-plugin-mysql.json.cfg.erb'
-  variables({
+  variables ({
     :name     => node.hostname,
     :metrics  => node['cog_newrelic']['plugin_mysql']['metrics'],
-    :user     => node['cog_newrelic']['user'],
+    :user     => node['cog_newrelic']['daemon_user'],
     :passwd   => mysql_access['newrelic']
   })
 
-  notifies :restart, 'runit_service[newrelic-plugin-mysql]'
+  notifies :restart, notifyname
   action :create
 end
 
@@ -73,12 +80,48 @@ template "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['c
     :log_path     => node['cog_newrelic']['plugin-log-path']
   })
 
-  notifies :restart, 'runit_service[newrelic-plugin-mysql]'
+  notifies :restart, notifyname
   action :create
 end
 
-runit_service 'newrelic-plugin-mysql' do
-  default_logger true
+if (node['cog_newrelic']['plugin_mysql']['init_style'] == 'systemd') then
 
-  action [ :enable, :start ]
+  # This is only needed at first run really
+  ohai "reload_passwd" do
+    action :reload
+    if (node['chef_packages']['ohai']['version'].to_i > 6) then
+      plugin "etc"
+    end
+  end
+
+  template '/usr/lib/systemd/system/newrelic-plugin-mysql.service' do
+    source 'newrelic_mysql_plugin.systemd.erb'
+    owner 'root'
+    group 'root'
+    mode '0644'
+    action :create
+    variables lazy {
+      {
+      :user => node['cog_newrelic']['daemon_user'],
+      :group => node['etc']['passwd'][node['cog_newrelic']['daemon_user']]['gid'],
+      :workdir => "#{node['cog_newrelic']['plugin-path']}/newrelic_mysql_plugin-#{node['cog_newrelic']['plugin_mysql']['version']}/"
+      }
+    }
+    notifies :run,'execute[systemd-reload]', :immediately
+  end
+
+  execute 'systemd-reload' do
+    command 'systemctl daemon-reload'
+
+    action :nothing
+  end
+  service 'newrelic-plugin-mysql' do
+    action [:enable, :start]
+  end
+else
+  runit_service 'newrelic-plugin-mysql' do
+    default_logger true
+
+    action [ :enable, :start ]
+  end
 end
